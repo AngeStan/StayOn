b466982fffd4b4c32b54e9c89ded4fd7071e75d6
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-21 14:05:58 +0100

    new: activation toggle

--- start.py
+++ start.py
@@ -1,13 +1,14 @@
-import pyautogui
-import time
+import pyautogui, time
 
 print('Welcom to "StayOn" program.')
 
-try:
-    while True:
-        pyautogui.moveTo(100, 0, duration=0.5)
-        time.sleep(5)
-        pyautogui.moveTo(100, 100, duration=0.5)
-        time.sleep(5)
-except KeyboardInterrupt:
-    print('Interrupted')
+while True:
+    input('To start the mouse movement, press Enter\nYou can stop it anytime with Ctrl+C')
+    try:
+        while True:
+            pyautogui.moveTo(100, 0, duration=0.5)
+            time.sleep(5)
+            pyautogui.moveTo(100, 100, duration=0.5)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print('Interrupted')
