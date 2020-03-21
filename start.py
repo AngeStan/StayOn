7fd9a3cb85c1d072ac5a0c1126414f11c0ce2d29
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-21 16:02:02 +0100

    new: start message; initial time before moving

--- start.py
+++ start.py
@@ -6,10 +6,12 @@ while True:
     input('\nTo start the mouse movement, press Enter\nYou can stop it anytime with Ctrl+C')
     try:
         while True:
+            print('\nStarted...')
+            time.sleep(10)
             pyautogui.move(0, 200, duration=0.5)
             pyautogui.leftClick()
             pyautogui.move(0, -200, duration=0.5)
             pyautogui.leftClick()
-            time.sleep(60)
+            time.sleep(50)
     except KeyboardInterrupt:
         print('\nInterrupted')
