4f00565016b72914d936a5253cc37df611f26f58
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-21 16:26:51 +0100

    fixed: "Started" message appearing in every cycle

--- start.py
+++ start.py
@@ -5,8 +5,8 @@ print('Welcome to "StayOn" program.\n')
 while True:
     input('\nTo start the mouse movement, press Enter\nYou can stop it anytime with Ctrl+C')
     try:
+        print('\nStarted...')
         while True:
-            print('\nStarted...')
             time.sleep(10)
             pyautogui.move(0, 200, duration=0.5)
             pyautogui.leftClick()
