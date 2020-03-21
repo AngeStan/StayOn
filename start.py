e26ab857c535dbcd378fd45f4bcd2d02b1278ec3
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-21 14:44:55 +0100

    new: click after slide

--- start.py
+++ start.py
@@ -1,13 +1,15 @@
 import pyautogui, time
 
-print('Welcom to "StayOn" program.')
+print('Welcome to "StayOn" program.')
 
 while True:
     input('To start the mouse movement, press Enter\nYou can stop it anytime with Ctrl+C')
     try:
         while True:
             pyautogui.move(0, 200, duration=0.5)
+            pyautogui.leftClick()
             pyautogui.move(0, -200, duration=0.5)
+            pyautogui.leftClick()
             time.sleep(2)
     except KeyboardInterrupt:
         print('Interrupted')
