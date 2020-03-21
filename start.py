a342819e75bdb7e116ba356c484c4c7366f1a9d7
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-21 14:39:12 +0100

    changed: relative movement

--- start.py
+++ start.py
@@ -6,9 +6,8 @@ while True:
     input('To start the mouse movement, press Enter\nYou can stop it anytime with Ctrl+C')
     try:
         while True:
-            pyautogui.moveTo(100, 0, duration=0.5)
-            time.sleep(5)
-            pyautogui.moveTo(100, 100, duration=0.5)
-            time.sleep(5)
+            pyautogui.move(0, 200, duration=0.5)
+            pyautogui.move(0, -200, duration=0.5)
+            time.sleep(2)
     except KeyboardInterrupt:
         print('Interrupted')
